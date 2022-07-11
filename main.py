4a4c0a9c0b35229e401e59ddffb9f44aca41b36f
Pedro <ponsvpedro@gmail.com>
2022-07-11 15:06:55 +0200

    More documentation

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import numpy as np
 
 def bandpass(sig, flim, dt=0.001):
     """
-    Simple bandpass filter
+    Simple bandpass filter.
+
+    - Input:
+
+        -`sig`: 1D array, signal.
+
+        -`flim`: Frequency limits (low, high)
+
+        -`dt`: Interval between samples (1/frequency)
     """
     ff = np.fft.rfft(sig)
     fr = np.fft.rfftfreq(len(sig), 0.001)
@@ -15,6 +23,14 @@ def bandpass(sig, flim, dt=0.001):
 def mat_bandpass(msig, flim, dt=0.001):
     """
     Simple bandpass filter for 2D arrays (along the second dimension)
+
+    - Input:
+
+        -`msig`: 2D array of signals.
+
+        -`flim`: Frequency limits (low, high)
+        
+        -`dt`: Interval between samples (1/frequency)
     """
     nmat = []
     for sig in msig:
@@ -26,6 +42,16 @@ def detect_peaks(arr, num=3):
     Simple peak-detecting algorithm for 2D arrays. 
 
     *Must be improved*
+
+    - Input:
+        
+        -`arr`: 2D array
+
+        -`num`: Number of peaks we want to find
+    
+    - Outputs:
+
+        -`peaks`: Array of peak indices
     """
     tmp = arr.copy()
     peaks = []
