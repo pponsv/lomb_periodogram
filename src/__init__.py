dfa7842005b92e67424c67f29a2d193ca3277f1c
Pedro <ponsvpedro@gmail.com>
2024-04-12 10:09:16 +0200

    Added easylombs to main

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from . import plots, synth, utils, lomb
 from .lomb_class import Lomb_vec
+from .lomb import easylomb2, easylomb3, easylomb3_difftimes
 
 __all__ = ["plots", "synth", "utils", "lomb", "Lomb_vec"]
