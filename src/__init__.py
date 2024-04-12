57731615f8724af30c1d225f1293c0433e5afb7c
Pedro <ponsvpedro@gmail.com>
2024-04-12 10:11:26 +0200

    fix

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,13 @@ from . import plots, synth, utils, lomb
 from .lomb_class import Lomb_vec
 from .lomb import easylomb2, easylomb3, easylomb3_difftimes
 
-__all__ = ["plots", "synth", "utils", "lomb", "Lomb_vec"]
+__all__ = [
+    "plots",
+    "synth",
+    "utils",
+    "lomb",
+    "Lomb_vec",
+    "easylomb2",
+    "easylomb3",
+    "easylomb3_difftimes",
+]
