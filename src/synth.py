273dc410f60623cc5125624c29d371e5e96c5892
Pedro <ponsvpedro@gmail.com>
2025-02-18 15:32:17 +0100

    Add Mode and Modes dataclasses for improved signal processing

--- src/synth.py
+++ src/synth.py
@@ -1,4 +1,37 @@
 import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class Mode:
+    m: int
+    n: int
+    f: float
+    amp: complex
+
+    def __call__(self):
+        return np.array([self.m, self.n, self.f, self.amp])
+
+
+@dataclass
+class Modes:
+    modes: list[Mode]
+
+    def __iter__(self):
+        return iter(self.modes)
+
+    def __add__(self, other):
+        if isinstance(other, Mode):
+            return Modes(self.modes + [other])
+        elif isinstance(other, Modes):
+            return Modes(self.modes + other.modes)
+
+    def __call__(self):
+        return np.array([mode() for mode in self.modes])
+
+    @classmethod
+    def from_array(cls, array):
+        return cls([Mode(*mode) for mode in array])
 
 
 def single_signal_noise(t, modes, theta, phi, anoise=0.2, pnoise=0.2):
@@ -9,15 +42,21 @@ def single_signal_noise(t, modes, theta, phi, anoise=0.2, pnoise=0.2):
 
         - `t`
     """
-    modes = np.array(modes)
-    assert modes.shape[1] == 4, "Modes not shaped well"
-    fac = np.sum(np.abs(modes[:, 3])) * anoise
+    if not isinstance(modes, Modes):
+        modes = Modes.from_array(modes)
+    fac = np.sum(np.abs([mode.amp for mode in modes])) * anoise
     sig = np.zeros(t.shape, dtype=np.complex128)
     for mode in modes:
-        m, n, f, amp = mode
+        # m, n, f, amp = mode()
         tmpnoise = 2 * np.pi * pnoise * np.random.normal(0, pnoise, t.shape)
-        sig += amp * np.exp(
-            1j * (m * theta + n * phi - 2 * np.pi * f * t + tmpnoise)
+        sig += mode.amp * np.exp(
+            1j
+            * (
+                mode.m * theta
+                + mode.n * phi
+                - 2 * np.pi * mode.f * t
+                + tmpnoise
+            )
         )
     return np.real(sig + (np.random.rand(len(t)) - 0.5) * fac)
 
